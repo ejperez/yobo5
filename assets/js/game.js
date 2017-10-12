df1d21ec717eaa7cf2ddaa87fc77aa755121f33d
EJ Perez <idyey04@gmail.com>
2017-10-12 22:01:07 +0800

    Added life lines

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,12 +4,25 @@ var Game = {};
 	var currentLevelVars = {
 		questionText: $('.question-text'),
 		prizeText: $('.prize-text'),
+		levelText: $('.level-text'),
 		questionChoices: $('.question-choices'),
 		currentAnswer: null,
 		currentQuestion: null
 	};
 
-	var maxLevel = 2;
+	var maxLevel = 1;
+	var imgWrong = $('.img-wrong');
+	var imgCorrect = $('.img-correct');
+	var btnLifelines = $('.btn-lifeline');
+	var btnResume = $('.btn-resume');
+	var ringing = $('.ringing');
+	var imagePath = 'assets/images/';
+
+	var friend = {
+		name: $('.friend-name'),
+		image: $('.friend-image'),
+		message: $('.friend-message')
+	};
 
 	Game.init = function () {
 		Game.currentLevel = 1;
@@ -20,7 +33,8 @@ var Game = {};
 			playing: $('.container-playing'),
 			correct: $('.container-correct'),
 			wrong: $('.container-wrong'),
-			won: $('.container-won')
+			won: $('.container-won'),
+			friend: $('.container-friend')
 		};
 
 		// Get levels data
@@ -28,28 +42,155 @@ var Game = {};
 			url: 'data/levels.json',
 			dataType: 'json',
 			success: function (data) {
-				Game.data = data;
+				Game.data.levels = data.levels;
+			}
+		});
+
+		// Get images data
+		$.get({
+			url: 'data/images.json',
+			dataType: 'json',
+			success: function (data) {
+				Game.data.images = data.images;
+			}
+		});
+
+		// Get friends data
+		$.get({
+			url: 'data/friends.json',
+			dataType: 'json',
+			success: function (data) {
+				Game.data.friends = data.friends;
 			}
 		});
 
 		// Event handlers
+		// Play button click
 		$(document).on('click', '.btn-play', function () {
 			Game.state = 'playing';
 			Game.currentLevel = 1;
+
+			// Hide other containers
 			Game.containers.welcome.addClass('hide');
 			Game.containers.wrong.addClass('hide');
+
+			// Reset lifelines
+			btnLifelines.prop('disabled', false);
+
 			Game.update();
 		});
 
+		// Choice click
 		$(document).on('click', '.btn-choice', function () {
 			Game.state = 'checking';
 			currentLevelVars.currentAnswer = $(this).data('value');
 			Game.update();
 		});
 
+		// Lifeline 50:50 button click
+		$(document).on('click', '.btn-5050', function () {
+			// Select two distinct choices to be removed
+			var choice1Index = currentLevelVars.currentQuestion.options[generateRandomInt(0, currentLevelVars.currentQuestion.options.length)];
+			var choice2Index = currentLevelVars.currentQuestion.options[generateRandomInt(0, currentLevelVars.currentQuestion.options.length)];
+
+			while (choice2Index === choice1Index) {
+				choice2Index = currentLevelVars.currentQuestion.options[generateRandomInt(0, currentLevelVars.currentQuestion.options.length)];
+			}
+
+			// Disabled them
+			currentLevelVars.questionChoices.find('.btn-choice[data-value="' + choice1Index + '"]').prop('disabled', true);
+			currentLevelVars.questionChoices.find('.btn-choice[data-value="' + choice2Index + '"]').prop('disabled', true);
+
+			// Disable lifeline
+			$(this).prop('disabled', true);
+		});
+
+		// Lifeline ask the audience button click
+		$(document).on('click', '.btn-ask', function () {
+			// Generate random vote counts
+			var remainingVotes = 100;
+
+			var choices = currentLevelVars.questionChoices.find('.btn-choice');
+			$.each(choices, function (index, value) {
+				if (index === choices.length - 1) {
+					var randomVoteCount = remainingVotes;
+				} else {
+					var randomVoteCount = generateRandomInt(1, remainingVotes);
+					remainingVotes -= randomVoteCount;
+				}
+
+				var html = $(value).html() + ' <span class="badge">' + randomVoteCount + ' vote(s)</span>';
+
+				$(value).html(html);
+			});
+
+			// Disable lifeline
+			$(this).prop('disabled', true);
+		});
+
+		// Lifeline call a friend button click
+		var callAFriendInterval, timeLimit = 10;
+		$(document).on('click', '.btn-call', function () {
+			// Select random friend
+			var selectedFriend = Game.data.friends[generateRandomInt(0, Game.data.friends.length)];
+
+			// Generate random choice
+			var choices = ['A', 'B', 'C', 'D'];
+			var randomChoiceIndex = generateRandomInt(0, choices.length);
+
+			// Show friend
+			friend.name.html(selectedFriend.name);
+			friend.image.prop('src', imagePath + selectedFriend.image);
+
+			// Replace choice placeholder
+			var message = selectedFriend.message.replace('{choice}', choices[randomChoiceIndex]);
+			friend.message.html(message);
+
+			Game.containers.playing.addClass('hide');
+			Game.containers.friend.removeClass('hide');
+
+			// Disable lifeline
+			$(this).prop('disabled', true);
+
+			ringing.fadeOut(3000, function () {
+				// Show fields
+				ringing.addClass('hide');
+				btnResume.removeClass('hide');
+				friend.name.removeClass('hide');
+				friend.image.removeClass('hide');
+				friend.message.removeClass('hide');
+
+				// Start timer
+				callAFriendInterval = setInterval(function () {
+					if (timeLimit < 0) {
+						resume();
+					} else {
+						btnResume.html('Back to question (' + timeLimit + ')');
+						timeLimit--;
+					}
+				}, 1000);
+			});
+		});
+
+		// Resume after call a friend
+		var resume = function () {
+			clearInterval(callAFriendInterval);
+			Game.status = 'resume';
+			Game.containers.friend.addClass('hide');
+			Game.update();
+		};
+
+		$(document).on('click', '.btn-resume', resume);
+
 		// Start when all AJAX calls are finished
 		$(document).ajaxStop(function () {
+			// Test
+			Game.state = 'won';
+			Game.currentLevel = 11;
+			Game.containers.welcome.addClass('hide');
 			Game.update();
+
+			// Game.update();
 		});
 	};
 
@@ -78,6 +219,8 @@ var Game = {};
 
 				break;
 			case 'correct':
+				var image = imagePath + Game.data.images.win[generateRandomInt(0, Game.data.images.win.length)];
+				imgCorrect.prop('src', image);
 				Game.containers.correct.removeClass('hide');
 
 				setTimeout(function () {
@@ -85,9 +228,11 @@ var Game = {};
 					Game.state = 'continue';
 					Game.currentLevel += 1;
 					Game.update();
-				}, 1000)
+				}, 2000)
 				break;
 			case 'wrong':
+				var image = imagePath + Game.data.images.lose[generateRandomInt(0, Game.data.images.lose.length)];
+				imgWrong.prop('src', image);
 				Game.containers.wrong.removeClass('hide');
 				break;
 			case 'continue':
@@ -105,6 +250,9 @@ var Game = {};
 			case 'won':
 				Game.containers.won.removeClass('hide');
 				break;
+			case 'resume':
+				Game.containers.playing.removeClass('hide');
+				break;
 		}
 	};
 
@@ -118,6 +266,9 @@ var Game = {};
 
 		// Show the prize
 		currentLevelVars.prizeText.html(level.prize);
+
+		// Show the current level
+		currentLevelVars.levelText.html('Level ' + Game.currentLevel + ' of ' + maxLevel);
 
 		// Select a question
 		var selectedQuestionIndex = generateRandomInt(0, level.questions.length);
@@ -145,7 +296,7 @@ var Game = {};
 		var alphabet = ['A.', 'B.', 'C.', 'D.'];
 
 		choices.forEach(function (choice, index) {
-			html += '<button style="white-space:normal" class="btn btn-default btn-block btn-choice" data-value="' + choice + '">' + alphabet[index] + '' + choice + '</button>';
+			html += '<button style="white-space:normal;text-align:left;" class="btn btn-default btn-block btn-choice" data-value="' + choice + '">' + alphabet[index] + ' ' + choice + '</button>';
 		});
 
 		return html;
